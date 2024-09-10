f6476291d9186b6c25f1e0badda4854603f5d177
Gealor <gzazulya@gmail.com>
2024-09-10 18:15:23 +0400

    InappropriateFunctionPointException extends Exception instead of IllegalArgumentException

---
+++ functions/exceptions/FunctionPointIndexOutOfBoundsException.java
@@ -0,0 +1,14 @@
+package functions.exceptions;
+
+public class FunctionPointIndexOutOfBoundsException extends IndexOutOfBoundsException{
+
+    // Конструктор по умолчанию
+    public FunctionPointIndexOutOfBoundsException(){
+        super();
+    }
+
+    // Конструктор с сообщением
+    public FunctionPointIndexOutOfBoundsException(String message) {
+        super(message);
+    }
+}
---
+++ functions/exceptions/InappropriateFunctionPointException.java
@@ -0,0 +1,12 @@
+package functions.exceptions;
+
+public class InappropriateFunctionPointException extends Exception{
+
+    public InappropriateFunctionPointException(){
+        super();
+    }
+
+    public InappropriateFunctionPointException(String message){
+        super(message);
+    }
+}
