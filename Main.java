e71c1c283f533766514af42ec43d291fbea6b111
Gealor <gzazulya@gmail.com>
2024-09-10 18:24:03 +0400

    InappropriateFunctionPointException extends Exception instead of IllegalArgumentException

--- Main.java
+++ Main.java
@@ -4,42 +4,42 @@ import functions.exceptions.InappropriateFunctionPointException;
 public class Main {
     public static void main(String[] args) {
         try {
-        double[] yValues = {-8.0, -6.0, -4.0, -2.0, 0.0, 2.0, 4.0, 6.0 ,8.0};
-        TabulatedFunction tf = new ArrayTabulatedFunction(-4.0, 4.0, yValues);
-        for (FunctionPoint point : tf.getPoints()){
-            System.out.println(point.getX() + " : " + point.getY());
-        }
-        System.out.println("Left border: " + tf.getLeftDomainBorder());
-        System.out.println("Right border: " + tf.getRightDomainBorder());
-        double x = 1.5;
-        System.out.println("Value of function for " + x + " : " + tf.getFunctionValue(x));
-        System.out.println("Count of dots: " + tf.getPointsCount());
+            double[] yValues = {-8.0, -6.0, -4.0, -2.0, 0.0, 2.0, 4.0, 6.0 ,8.0};
+            TabulatedFunction tf = new ArrayTabulatedFunction(-4.0, 4.0, yValues);
+            for (FunctionPoint point : tf.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("Left border: " + tf.getLeftDomainBorder());
+            System.out.println("Right border: " + tf.getRightDomainBorder());
+            double x = 1.5;
+            System.out.println("Value of function for " + x + " : " + tf.getFunctionValue(x));
+            System.out.println("Count of dots: " + tf.getPointsCount());
 
-        for (int index = 0; index< tf.getPointsCount(); index++){
-            FunctionPoint point=tf.getPoint(index);
-            System.out.println("Dot with getPoint method : " + "( " + point.getX() + " : " + point.getY() + " )");
-        }
+            for (int index = 0; index< tf.getPointsCount(); index++){
+                FunctionPoint point=tf.getPoint(index);
+                System.out.println("Dot with getPoint method : " + "( " + point.getX() + " : " + point.getY() + " )");
+            }
 
-        tf.setPoint(4, new FunctionPoint(-0.5,0));
-        for (FunctionPoint point : tf.getPoints()){
-            System.out.println(point.getX() + " : " + point.getY());
-        }
-        System.out.println("X Coordinate for index " + 4 + " : " + tf.getPointX(4));
-        System.out.println("Y Coordinate for index " + 4 + " : " + tf.getPointY(4));
-
-
-        tf.deletePoint(4);
-        for (FunctionPoint point : tf.getPoints()){
-            System.out.println(point.getX() + " : " + point.getY());
-        }
-        System.out.println("Count of dots: " + tf.getPointsCount());
+            tf.setPoint(4, new FunctionPoint(-0.5,0));
+            for (FunctionPoint point : tf.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("X Coordinate for index " + 4 + " : " + tf.getPointX(4));
+            System.out.println("Y Coordinate for index " + 4 + " : " + tf.getPointY(4));
 
 
-        tf.addPoint(new FunctionPoint(0,0));
-        for (FunctionPoint point : tf.getPoints()){
-            System.out.println(point.getX() + " : " + point.getY());
-        }
-        System.out.println("Count of dots: " + tf.getPointsCount());
-    } catch (InappropriateFunctionPointException e) { System.out.println("Exception" + e); }
+            tf.deletePoint(4);
+            for (FunctionPoint point : tf.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("Count of dots: " + tf.getPointsCount());
+
+
+            tf.addPoint(new FunctionPoint(0,0));
+            for (FunctionPoint point : tf.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("Count of dots: " + tf.getPointsCount());
+        } catch (InappropriateFunctionPointException e) { System.out.println("Exception" + e); }
     }
 }
