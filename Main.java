cdeda3b56fc55b88aa6a98e6889249411ac222a5
Gealor <gzazulya@gmail.com>
2024-09-15 14:44:34 +0400

    Init Commit

--- Main.java
+++ Main.java
@@ -1,42 +1,88 @@
 import functions.*;
-
+import functions.exceptions.InappropriateFunctionPointException;
 public class Main {
     public static void main(String[] args) {
-        double[] yValues = {-8.0, -6.0, -4.0, -2.0, 0.0, 2.0, 4.0, 6.0 ,8.0};
-        TabulatedFunction tf = new TabulatedFunction(-4.0, 4.0, yValues);
-        for (FunctionPoint point : tf.getPoints()){
-            System.out.println(point.getX() + " : " + point.getY());
-        }
-        System.out.println("Left border: " + tf.getLeftDomainBorder());
-        System.out.println("Right border: " + tf.getRightDomainBorder());
-        double x = 1.5;
-        System.out.println("Value of function for " + x + " : " + tf.getFunctionValue(x));
-        System.out.println("Count of dots: " + tf.getPointsCount());
+        // Массив
+        System.out.println("\nМассив\n");
+        try {
+            double[] yValues = {-8.0, -6.0, -4.0, -2.0, 0.0, 2.0, 4.0, 6.0 ,8.0};
+            TabulatedFunction tf1 = new ArrayTabulatedFunction(-4.0, 4.0, yValues);
+            for (FunctionPoint point : tf1.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("Left border: " + tf1.getLeftDomainBorder());
+            System.out.println("Right border: " + tf1.getRightDomainBorder());
+            double x = 1.5;
+            System.out.println("Value of function for " + x + " : " + tf1.getFunctionValue(x));
+            System.out.println("Count of dots: " + tf1.getPointsCount());
 
-        for (int index = 0; index< tf.getPointsCount(); index++){
-            FunctionPoint point=tf.getPoint(index);
-            System.out.println("Dot with getPoint method : " + "( " + point.getX() + " : " + point.getY() + " )");
-        }
+            for (int index = 0; index< tf1.getPointsCount(); index++){
+                FunctionPoint point=tf1.getPoint(index);
+                System.out.println("Dot with getPoint method : " + "( " + point.getX() + " : " + point.getY() + " )");
+            }
 
-        tf.setPoint(4, new FunctionPoint(-0.5,0));
-        for (FunctionPoint point : tf.getPoints()){
-            System.out.println(point.getX() + " : " + point.getY());
-        }
-        System.out.println("X Coordinate for index " + 4 + " : " + tf.getPointX(4));
-        System.out.println("Y Coordinate for index " + 4 + " : " + tf.getPointY(4));
-
-
-        tf.deletePoint(4);
-        for (FunctionPoint point : tf.getPoints()){
-            System.out.println(point.getX() + " : " + point.getY());
-        }
-        System.out.println("Count of dots: " + tf.getPointsCount());
+            tf1.setPoint(4, new FunctionPoint(-0.5,0));
+            for (FunctionPoint point : tf1.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("X Coordinate for index " + 4 + " : " + tf1.getPointX(4));
+            System.out.println("Y Coordinate for index " + 4 + " : " + tf1.getPointY(4));
 
 
-        tf.addPoint(new FunctionPoint(0,0));
-        for (FunctionPoint point : tf.getPoints()){
-            System.out.println(point.getX() + " : " + point.getY());
-        }
-        System.out.println("Count of dots: " + tf.getPointsCount());
+            tf1.deletePoint(4);
+            for (FunctionPoint point : tf1.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("Count of dots: " + tf1.getPointsCount());
+
+
+            tf1.addPoint(new FunctionPoint(0,0));
+            for (FunctionPoint point : tf1.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("Count of dots: " + tf1.getPointsCount());
+        } catch (InappropriateFunctionPointException e) { System.out.println("Exception" + e); }
+
+        // Связанный список
+        System.out.println("\nСвязанный список\n");
+
+        try {
+            double[] yValues = {-8.0, -6.0, -4.0, -2.0, 0.0, 2.0, 4.0, 6.0 ,8.0};
+            TabulatedFunction tf = new LinkedListTabulatedFunction(-4.0, 4.0, yValues);
+            for (FunctionPoint point : tf.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("Left border: " + tf.getLeftDomainBorder());
+            System.out.println("Right border: " + tf.getRightDomainBorder());
+            double x = 1.5;
+            System.out.println("Value of function for " + x + " : " + tf.getFunctionValue(x));
+            System.out.println("Count of dots: " + tf.getPointsCount());
+
+            for (int index = 0; index< tf.getPointsCount(); index++){
+                FunctionPoint point=tf.getPoint(index);
+                System.out.println("Dot with getPoint method : " + "( " + point.getX() + " : " + point.getY() + " )");
+            }
+
+            tf.setPoint(4, new FunctionPoint(-0.5,0));
+            for (FunctionPoint point : tf.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("X Coordinate for index " + 4 + " : " + tf.getPointX(4));
+            System.out.println("Y Coordinate for index " + 4 + " : " + tf.getPointY(4));
+
+
+            tf.deletePoint(4);
+            for (FunctionPoint point : tf.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("Count of dots: " + tf.getPointsCount());
+
+
+            tf.addPoint(new FunctionPoint(0,0));
+            for (FunctionPoint point : tf.getPoints()){
+                System.out.println(point.getX() + " : " + point.getY());
+            }
+            System.out.println("Count of dots: " + tf.getPointsCount());
+        } catch (InappropriateFunctionPointException e) { System.out.println("Exception" + e); }
     }
 }
--- functions/TabulatedFunction.java
+++ functions/TabulatedFunction.java
@@ -1,171 +1,36 @@
 package functions;
 
-public class TabulatedFunction{
-    private FunctionPoint[] points;
-    
+import functions.exceptions.FunctionPointIndexOutOfBoundsException;
+import functions.exceptions.InappropriateFunctionPointException;
 
-    // Конструктор, создающий табулированную функцию с заданным количеством точек
-    public TabulatedFunction(double leftX, double rightX, int pointsCount) {
-        if (pointsCount < 2 || leftX >= rightX) {
-            throw new IllegalArgumentException("Invalid parameters for TabulatedFunction constructor.");
-        }
-        points = new FunctionPoint[pointsCount];
-        // Вычисляю интервал для размещения точки по координате x
-        //                левая граница - правая граница / количество интервалов(которых на 1 единицу меньше точек!!!!!)
-        double interval = (rightX - leftX) / (pointsCount - 1);
-        for (int i = 0; i < pointsCount; i++) {
-            points[i] = new FunctionPoint(leftX + i * interval, 0);
-        }
-    }
+public interface TabulatedFunction extends Function{
 
-    // Конструктор, создающий табулированную функцию с заданными значениями функции
-    public TabulatedFunction(double leftX, double rightX, double[] values) {
-        int pointsCount = values.length;
-
-        if (pointsCount < 2) {
-            throw new IllegalArgumentException("Values array should contain at least 2 elements.");
-        }
-        
-        points = new FunctionPoint[pointsCount];
-        
-        double interval = (rightX - leftX) / (pointsCount - 1);
-        for (int i = 0; i < pointsCount; i++) {
-            points[i] = new FunctionPoint(leftX + i * interval, values[i]);
-        }
-    }
-
-    public FunctionPoint[] getPoints() {
-        return points;
-    }
-
-    public double getLeftDomainBorder(){
-        return points[0].getX();
-    }
-
-    public double getRightDomainBorder() {
-        return points[points.length - 1].getX();
-    }
-
-    // Метод для получения значения функции в точке x
-    public double getFunctionValue(double x) {
-        // Если x вне границ области определения, возвращаю NaN
-        if (x < getLeftDomainBorder() || x > getRightDomainBorder()) {
-            return Double.NaN;
-        }
-
-        // Линейная интерполяция между точками
-        for (int i = 0; i < points.length - 1; i++) {
-            double x1 = points[i].getX();
-            double x2 = points[i + 1].getX();
-            if (x1 <= x && x <= x2) {
-                double y1 = points[i].getY();
-                double y2 = points[i + 1].getY();
-                return y1 + (x - x1) * (y2 - y1) / (x2 - x1);
-            }
-        }
-
-        // На случай, если x точно совпадает с координатой любой из точек
-        for (FunctionPoint point : points) {
-            if (point.getX() == x) {
-                return point.getY();
-            }
-        }
-
-        // По умолчанию возвращаю NaN из класса Double
-        return Double.NaN;
-    }
+    public FunctionPoint[] getPoints();
 
     // Метод для получения количества точек
-    public int getPointsCount() {
-        return points.length;
-    }
+    public int getPointsCount();
 
     // Метод для получения точки по индексу
-    public FunctionPoint getPoint(int index) {
-        if (index < 0 || index >= points.length) {
-            throw new IndexOutOfBoundsException("Invalid index: " + index);
-        }
-        return points[index];
-    }
+    public FunctionPoint getPoint(int index);
 
     // Метод для замены точки по индексу
-    public void setPoint(int index, FunctionPoint point) {
-        if (index < 0 || index >= points.length) {
-            throw new IndexOutOfBoundsException("Invalid index: " + index);
-        }
+    public void setPoint(int index, FunctionPoint point) throws InappropriateFunctionPointException;
 
-        // Проверяю точку, чтобы она соответствовала порядку точек в массиве
-        if (index > 0 && point.getX() <= points[index - 1].getX() ||
-            index < points.length - 1 && point.getX() >= points[index + 1].getX()) {
-            throw new IllegalArgumentException("Invalid x coordinate: " + point.getX());
-        }
-        points[index] = point;
-    }
-
-    public double getPointX(int index) {
-        return getPoint(index).getX();
-    }
+    public double getPointX(int index);
 
     // Метод для установки значения x точки по индексу
-    public void setPointX(int index, double x) {
-        if (index < 0 || index >= points.length) {
-            throw new IndexOutOfBoundsException("Invalid index: " + index);
-        }
-        if (index > 0 && x <= points[index - 1].getX() ||
-            index < points.length - 1 && x >= points[index + 1].getX()) {
-            throw new IllegalArgumentException("Invalid x coordinate: " + x);
-        }
-        points[index].setX(x);
-    }
+    public void setPointX(int index, double x) throws InappropriateFunctionPointException;
 
     // Метод для получения значения y точки по индексу
-    public double getPointY(int index) {
-        return getPoint(index).getY();
-    }
+    public double getPointY(int index);
 
     // Метод для установки значения y точки по индексу
-    public void setPointY(int index, double y) {
-        if (index < 0 || index >= points.length) {
-            throw new IndexOutOfBoundsException("Invalid index: " + index);
-        }
-        points[index].setY(y);
-    }
+    public void setPointY(int index, double y);
 
     // Метод для удаления точки по индексу
-    public void deletePoint(int index) {
-        int count = getPointsCount();
-        if (index < 0 || index >= points.length) {
-            throw new IndexOutOfBoundsException("Invalid index: " + index);
-        }
-        if (count <= 2) {
-            throw new IllegalStateException("Cannot delete point. At least two points must remain.");
-        }
-        FunctionPoint[] newPoints = new FunctionPoint[getPointsCount()-1];
-        // Копирую элементы массива, начиная с точки index + 1, на позицию index, тем самым сдвигая все элементы влево и удаляя элемент на позиции index.
-        System.arraycopy(points, 0, newPoints, 0, count - index);
-        System.arraycopy(points, index + 1, newPoints, index, count - index - 1);
-        this.points = newPoints;
-    }
+    public void deletePoint(int index);
 
     // Метод для добавления точки
-    public void addPoint(FunctionPoint point) {
-        if (points.length > 0) {
-            if (point.getX() <= points[0].getX() || point.getX() >= points[points.length - 1].getX()) {
-                throw new IllegalArgumentException("Point x-coordinate must be within existing points' x-range.");
-            }
-        }
-        FunctionPoint[] newPoints = new FunctionPoint[getPointsCount() + 1];
-        int i = 0;
-        while (i < points.length && points[i].getX() < point.getX()) {
-            newPoints[i] = points[i];
-            i++;
-        }
-        newPoints[i] = point;
-        while (i < points.length) {
-            newPoints[i + 1] = points[i];
-            i++;
-        }
-        this.points = newPoints;
-    }
+    public void addPoint(FunctionPoint point) throws InappropriateFunctionPointException;
 }
 
