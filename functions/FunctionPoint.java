451c37efe198f0d934bb824828716842ffc5fd08
Gealor <gzazulya@gmail.com>
2024-09-19 22:21:23 +0400

    Second version with Cycled List

---
+++ functions/FunctionPoint.java
@@ -0,0 +1,37 @@
+package functions;
+
+public class FunctionPoint{
+    private double x;
+    private double y;
+
+    public double getX(){
+        return this.x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
+
+    public void setX(double ox){
+        this.x = ox;
+    }
+
+    public void setY(double oy){
+        this.y = oy;
+    }
+    
+    public FunctionPoint(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public FunctionPoint(double ox, double oy){
+        this.x = ox;
+        this.y = oy;
+    }
+
+    public FunctionPoint(FunctionPoint point){
+        this.x=point.getX();
+        this.y=point.getY();
+    }
+}
---
+++ functions/TabulatedFunction.java
@@ -0,0 +1,43 @@
+package functions;
+
+import functions.exceptions.FunctionPointIndexOutOfBoundsException;
+import functions.exceptions.InappropriateFunctionPointException;
+
+public interface TabulatedFunction{
+
+    public FunctionPoint[] getPoints();
+
+    public double getLeftDomainBorder();
+
+    public double getRightDomainBorder();
+
+    // Метод для получения значения функции в точке x
+    public double getFunctionValue(double x);
+
+    // Метод для получения количества точек
+    public int getPointsCount();
+
+    // Метод для получения точки по индексу
+    public FunctionPoint getPoint(int index);
+
+    // Метод для замены точки по индексу
+    public void setPoint(int index, FunctionPoint point) throws InappropriateFunctionPointException;
+
+    public double getPointX(int index);
+
+    // Метод для установки значения x точки по индексу
+    public void setPointX(int index, double x) throws InappropriateFunctionPointException;
+
+    // Метод для получения значения y точки по индексу
+    public double getPointY(int index);
+
+    // Метод для установки значения y точки по индексу
+    public void setPointY(int index, double y);
+
+    // Метод для удаления точки по индексу
+    public void deletePoint(int index);
+
+    // Метод для добавления точки
+    public void addPoint(FunctionPoint point) throws InappropriateFunctionPointException;
+}
+
